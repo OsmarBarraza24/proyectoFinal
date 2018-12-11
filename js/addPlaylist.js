89b7fe29192c8b4b8df3732e877f1367e91a0f88
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-12-10 19:11:16 -0700

    Todo menso el Chuy
    Hehehe

--- js/addPlaylist.js
+++ js/addPlaylist.js
@@ -3,5 +3,5 @@ var lista = document.getElementById("AgregarPlaylist");
 lista.addEventListener("click", addForm());
 
 function addForm(){
-    lista.innerHTML = ['<form action = "music.php" method = "post"><input type="text" name="nombrePalylist"></form>'].join('');
+    lista.innerHTML = ['<form action = "music.php" method = "post"><input type="text" name="nombrePalylist"></form>'].join();
 }
