5abed0c6fedddb0c59a8c91090b864766074b8bf
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-12-11 17:43:05 -0600

    update user arreglado

--- js/index.js
+++ js/index.js
@@ -1,26 +1,19 @@
 function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-
-    // Loop through the FileList and render image files as thumbnails.
+    var files = evt.target.files;
     for (var i = 0, f; f = files[i]; i++) {
-      // Only process image files.
       if (!f.type.match('image.*')) {
         continue;
       }
 
       var reader = new FileReader();
-
-      // Closure to capture the file information.
       reader.onload = (function(theFile) {
         return function(e) {
-          // Render thumbnail.
           var span = document.getElementById('span');
           span.innerHTML = ['<img class="circle" src="', e.target.result,
                             '" title="', escape(theFile.name), '"/>'].join('');
         };
       })(f);
 
-      // Read in the image file as a data URL.
       reader.readAsDataURL(f);
     }
   }
