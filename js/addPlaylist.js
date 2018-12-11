409eeb28e91d7cffdfe187c043981c572bc17b22
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-12-11 16:32:15 -0600

    Funcinalidad

--- js/addPlaylist.js
+++ js/addPlaylist.js
@@ -1,11 +1,18 @@
 var lista = document.getElementById("AgregarPlaylist");
+var texto = document.getElementById("txtAdd");
 var bandera = false;
 lista.addEventListener("click", addForm);
+texto.addEventListener("lostpointercapture", quitForm);
 
 function addForm(){
     if (!bandera) {
-        lista.innerHTML = ['<form action = "music.php" method = "post"><input type="text" name="nombrePlaylist" class="inputPlaylist"></form>'].join();
+        lista.innerHTML = ['<form action = "music.php" method = "post"><input type="text" name="nombrePlaylist" class="inputPlaylist" id = "txtAdd"></form>'].join();
         bandera = true;
         
     }
+}
+
+function quitForm(){
+    lista.innerHTML = ['<i class="fas fa-plus-circle"></i>Añadir playlist'].join('');
+    bandera = false;
 }
