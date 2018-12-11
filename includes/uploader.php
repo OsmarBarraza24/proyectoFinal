c4d0f26b8732eb045f042cba54fde96a7881e449
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-12-10 17:01:52 -0700

    Pequeño cambio

--- includes/uploader.php
+++ includes/uploader.php
@@ -1,11 +1,5 @@
 <?php 
-$target_path = "";
-if($_POST['tipo'] == "imagenes"){
 $target_path = "imagenes/";
-}else{
-    header("Location:index.php");
-}
-$msg = " ";
 $target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
 
 $uploadedfileload=true;
@@ -18,14 +12,14 @@ echo $_FILES["uploadedfile"]["name"]. "<br>" . $uploadedFileType;
 
 if ($uploadedfile_size>1000000){
 
-$msg = $msg . "El archivo es mayor que 1MB, debes reduzcirlo antes de subirlo<BR>";
+$error[] = "El archivo es mayor que 1MB, debes reduzcirlo antes de subirlo<BR>";
 
 $uploadedfileload=false;
 
 }
 
 if (!($uploadedFileType == "image/jpeg" OR $uploadedFileType =="image/gif")){
-    $msg = $msg . " Tu archivo tiene que ser JPG o GIF. Otros archivos no son permitidos<BR>";
+    $error[] =  " Tu archivo tiene que ser JPG o GIF. Otros archivos no son permitidos<BR>";
     
     $uploadedfileload=false;
 }
@@ -33,7 +27,7 @@ if (!($uploadedFileType == "image/jpeg" OR $uploadedFileType =="image/gif")){
 
 $file_name=$_FILES["uploadedfile"]["name"];
 
-$add="uploads/$file_name";
+$add="imagenes/$file_name";
 
 if($uploadedfileload){
 
@@ -41,8 +35,6 @@ if(move_uploaded_file ($_FILES["uploadedfile"]["tmp_name"], $add)){
 echo " Ha sido subido satisfactoriamente";
 }
 
-}else{
-    echo $msg;
 }
 
 ?>
--- step.php
+++ step.php
@@ -10,42 +10,7 @@ if(isset($_POST['sent'])) {
 			if($caca == "") { $error[] = "La caja $calzon debe contener un valor"; }		
 	}
 
-  $target_path = "imagenes/";
-$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
-
-$uploadedfileload=true;
-
-$uploadedfile_size=$_FILES['uploadedfile']['size'];
-$uploadedFileType = $_FILES['uploadedfile']['type'];
-
-
-
-if ($uploadedfile_size>1000000){
-
-$error[] = "El archivo es mayor que 1MB, debes reduzcirlo antes de subirlo<BR>";
-
-$uploadedfileload=false;
-
-}
-
-if (!($uploadedFileType == "image/jpeg" OR $uploadedFileType =="image/gif")){
-  $error[] = " Tu archivo tiene que ser JPG o GIF. Otros archivos no son permitidos<BR>";
-    
-    $uploadedfileload=false;
-}
-
-
-$file_name="imagenes/$file_name";
-
-$add="imagenes/$file_name";
-
-if($uploadedfileload){
-
-if(move_uploaded_file ($_FILES["uploadedfile"]["tmp_name"], $add)){
-    echo " Ha sido subido satisfactoriamente";
-}
-
-}
+  include("includes/uploader.php");
 
 	if(!isset($error)) {
 		$queryUserUpdate = sprintf("UPDATE usuario SET nombre = '%s', apellidos = '%s' foto = '%s' genero = '%s'WHERE id = '%d'",
