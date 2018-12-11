896143149f6a8db85230af6d6c5f56295e59754a
OsmarElProgramador <barrazita11@gmail.com>
2018-12-10 19:12:16 -0700

    Commit que te quita l sueños
    nomas

--- updateuser.php
+++ updateuser.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/step.css">
     <link rel="stylesheet" href="css/update.css">
+
     <title>Document</title>
 </head>
 <body>
@@ -20,19 +21,11 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
+          <div class="cont"><img class="img" src="imagenes/Osmar.jpg" alt=""> <p style="color:white">Osmar Barraza Flores</p></div>
       </ul>
       <ul class="nav navbar-nav navbar-center">
         <form class="form-inline my-2 my-lg-0">
-
           <input class="form-control mr-sm-2 input-field" type="search" placeholder="Buscar" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0 input-button" type="submit"><i class="fas fa-search"></i>
-            Buscar</button>
         </form>
       </ul>
 
@@ -58,8 +51,27 @@
                 <label for="">Apellidos</label> <br>
                 <input type="text"> <br>
             </div>
-         </div>
-    </div>
+            <br>
+        </div>
+        <br>
+        <div class="container-fluid">
+        <div class="row justify-content-center">
+                    <div class="col-xs-12">
+                        <h2>¡Conoce las ventajas de unirte al premium!</h2>
+                    </div>
+                </div>
+                <div class="row box" style="text-align:center">
+                    <div class="col-xs-4">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, suscipit a. Numquam quas blanditiis veniam quam, hic cumque molestiae nostrum vero inventore recusandae, architecto fuga neque sed. Consequuntur, odit voluptatum?</p>
+                    </div>
+                    <div class="col-xs-4">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, suscipit a. Numquam quas blanditiis veniam quam, hic cumque molestiae nostrum vero inventore recusandae, architecto fuga neque sed. Consequuntur, odit voluptatum?</p>
+                    </div>
+                      <div class="col-xs-4">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, suscipit a. Numquam quas blanditiis veniam quam, hic cumque molestiae nostrum vero inventore recusandae, architecto fuga neque sed. Consequuntur, odit voluptatum?</p>
+                    </div>
+                </div>
+        </div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
