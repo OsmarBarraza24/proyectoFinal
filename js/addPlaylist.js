6a68ab29af3b090764cbaab19cc2281e7bae576f
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-12-10 19:28:03 -0700

    deberia de funcionar

--- js/addPlaylist.js
+++ js/addPlaylist.js
@@ -1,8 +1,11 @@
 var lista = document.getElementById("AgregarPlaylist");
-
+var bandera = false;
 lista.addEventListener("click", addForm);
 
 function addForm(){
-    lista.innerHTML = ['<form action = "music.php" method = "post"><input type="text" name="nombrePalylist"></form>'].join();
-    lista.id = "presionado";
+    if (!bandera) {
+        lista.innerHTML = ['<form action = "music.php" method = "post"><input type="text" name="nombrePalylist"></form>'].join();
+        bandera = true;
+        
+    }
 }
