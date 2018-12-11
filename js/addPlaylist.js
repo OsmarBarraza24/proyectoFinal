80224a4b6e376cfa3b60c6eec718b30cafb0f16c
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-12-10 19:15:53 -0700

    Equis de

--- js/addPlaylist.js
+++ js/addPlaylist.js
@@ -1,6 +1,6 @@
 var lista = document.getElementById("AgregarPlaylist");
 
-lista.addEventListener("change", addForm());
+lista.addEventListener("click", addForm);
 
 function addForm(){
     lista.innerHTML = ['<form action = "music.php" method = "post"><input type="text" name="nombrePalylist"></form>'].join();
