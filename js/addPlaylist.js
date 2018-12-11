305c10708a090d4b3f86bc7245e46f6b577060bf
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-12-10 19:13:47 -0700

    Demonios

--- js/addPlaylist.js
+++ js/addPlaylist.js
@@ -1,6 +1,6 @@
 var lista = document.getElementById("AgregarPlaylist");
 
-lista.addEventListener("click", addForm());
+lista.addEventListener("change", addForm());
 
 function addForm(){
     lista.innerHTML = ['<form action = "music.php" method = "post"><input type="text" name="nombrePalylist"></form>'].join();
