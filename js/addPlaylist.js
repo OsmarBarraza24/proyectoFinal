34511a8e26f85185e259283cc301e6c5e64e8bf3
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-12-10 19:06:06 -0700

    Espero que funcione

---
+++ js/addPlaylist.js
@@ -0,0 +1,7 @@
+var lista = document.getElementById("AgregarPlaylist");
+
+lista.addEventListener("click", addForm());
+
+function addForm(){
+    lista.innerHTML = ['<form action = "music.php" method = "post"><input type="text" name="nombrePalylist"></form>'].join('');
+}
--- music.php
+++ music.php
@@ -39,7 +39,7 @@ if(isset($_POST["logout"])){
           </ul> 
           <h6>TUS PLAYLIST</h6> 
           <ul>
-            <li><i class="fas fa-plus-circle"></i><a href=""> Añadir Playlist</a></li>
+            <li id = "AgregarPlaylist"><i class="fas fa-plus-circle"></i><a href=""> Añadir Playlist</a></li>
           </ul>       
       </div>
     </div>
