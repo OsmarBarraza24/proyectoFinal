67f814ba196a15342b9bb064a0b892e2d2147c33
OsmarElProgramador <barrazita11@gmail.com>
2018-12-12 15:33:02 -0700

    tititutut
    123

--- adminadd.php
+++ adminadd.php
@@ -83,7 +83,7 @@ session_start();
                 <form enctype = "multipart/form-data" method="post" action="adminadd.php">
                   <label for="nombre">Nombre</label> <br>
                   <input type="text" name="nombre"> <br>
-                  <input type="submit" value="Agregar" name="enviarA"> 
+                  <input style="margin-left:50px;margin-top:15px;" class="btn btn-primary" type="submit" value="Agregar" name="enviarA"> 
                 </form>
               </div>
             </div>
@@ -100,7 +100,13 @@ session_start();
     </div>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
       <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <img class="album" src="imagenes/Osmar.jpg" alt="">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
