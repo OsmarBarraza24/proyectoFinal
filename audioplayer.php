374ffbf6012150a1e5e6d6e49b222faf3adc76d5
OsmarElProgramador <barrazita11@gmail.com>
2018-12-13 04:59:38 -0700

    Ahhhh
    Por si

--- audioplayer.php
+++ audioplayer.php
@@ -2,7 +2,7 @@
 session_start();
 include("conexion/conexionBd.php");
 if(isset($_GET["playlist"])){
-    $queryObtenerPlaylistCancionAlbumArtista = "SELECT playlist.id, playlist.nombre, cancion.id, cancion.nombre
+    $queryObtenerPlaylistCancionAlbumArtista = "SELECT playlist.id, playlist.nombre, cancion.id, cancion.nombre, cancion.ruta
     FROM rel_cancion_playlist
     INNER JOIN cancion on rel_cancion_playlist.idCancion = cancion.id
     INNER JOIN playlist on rel_cancion_playlist.idPLaylist = playlist.id
@@ -12,21 +12,7 @@ if(isset($_GET["playlist"])){
     $resQueryObtenerPlaylistCancion = mysql_query($queryObtenerPlaylistCancionAlbumArtista, $conexionBd) or die ("Nelson brother");
     $dataPlaylistCancion = mysql_fetch_array($resQueryObtenerPlaylistCancion);
     
-    $queryObtenerAlbum = "SELECT album.id, album.nombre
-    FROM rel_cancion_album
-    INNER JOIN album on rel_cancion_album.idAlbum = album.id
-    WHERE rel_cancion_album.idCancion =
-    ". $dataPlaylistCancion[2];
-    $resQueryObtenerAlbum = mysql_query($queryObtenerAlbum, $conexionBd) or die ("Nel plomo");
-    $dataAlbum = mysql_fetch_array($resQueryObtenerAlbum);
-
-    $queryObtenerArtista = "SELECT artista.id, artista.nombre
-    FROM album
-    INNER JOIN artista on album.idArtista = artista.id
-    WHERE album.id =
-    ". $dataAlbum[0];
-
-    $resQueryObtenerArtista = mysql_query($queryObtenerArtista, $conexionBd) or die ("Nelson mandela");
+    
 
 
 }else {
@@ -42,8 +28,8 @@ if(isset($_GET["playlist"])){
     <meta name="viewport" content="width=device-width">
     <title>Reproductor</title>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <!-- Insert to your webpage before the </head> -->
     <script src="audioplayerengine/jquery.js"></script>
     <script src="audioplayerengine/amazingaudioplayer.js"></script>
@@ -94,9 +80,28 @@ if(isset($_GET["playlist"])){
     <!-- Insert to your webpage where you want to display the audio player -->
     <div id="amazingaudioplayer-1" style="display:block;position:relative;width:100%;height:auto;margin:0px auto 0px;">
         <ul class="amazingaudioplayer-audios" style="display:none;">
-            <li data-artist="" data-title="Love of Lesbian - Bajo el Volcán (Videoclip Oficial)" data-album="" data-info="" data-image="" data-duration="344">
-                <div class="amazingaudioplayer-source" data-src="audios/BajoElVolcan.mp3" data-type="audio/mpeg"></div>
+        <?php while($dataPlaylistCancion = mysql_fetch_array($resQueryObtenerPlaylistCancion)){
+            $queryObtenerAlbum = "SELECT album.id, album.nombre, album.imagen
+            FROM rel_cancion_album
+            INNER JOIN album on rel_cancion_album.idAlbum = album.id
+            WHERE rel_cancion_album.idCancion =
+            ". $dataPlaylistCancion[2];
+            $resQueryObtenerAlbum = mysql_query($queryObtenerAlbum, $conexionBd) or die ("Nel plomo");
+            $dataAlbum = mysql_fetch_array($resQueryObtenerAlbum);
+        
+            $queryObtenerArtista = "SELECT artista.id, artista.nombre
+            FROM album
+            INNER JOIN artista on album.idArtista = artista.id
+            WHERE album.id =
+            ". $dataAlbum[0];
+            
+            $resQueryObtenerArtista = mysql_query($queryObtenerArtista, $conexionBd) or die ("Nelson mandela");
+            $dataArtista = mysql_fetch_array($resQueryObtenerArtista);
+        ?>
+            <li data-artist=<?php echo '"'. $dataArtista[1] .'"'?> data-title=<?php echo '"'. $dataPlaylistCancion[3] .'"'?> data-album=<?php echo '"'. $dataAlbum[1] . '"'?> data-info="" data-image=<?php echo '"imagenes/'. $dataAlbum[2] .'"'?>>
+                <div class="amazingaudioplayer-source" data-src=<?php echo '"audios/'. $dataPlaylistCancion[4] .'"'?> data-type="audio/mpeg"></div>
             </li>
+        <?php }?>
         </ul>
 
     </div>
